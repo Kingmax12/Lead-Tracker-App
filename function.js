eb1eb9b6fcdcf475d34a7e563f4898378846d6b2
Kingmax12 <oyedeletope559@gmail.com>
2026-07-30 15:04:48 +0100

    Add databaseURL to Firebase config
    Added databaseURL to Firebase configuration.

--- function.js
+++ function.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
   messagingSenderId: "487274490195",
   appId: "1:487274490195:web:305e7ddb4af507eea459a6",
   measurementId: "G-3Q36PFPEYZ"
+ databaseURL: "https://leads-tracker-app-44d59.firebaseapp.com"
 };
 
 // Initialize Firebase
