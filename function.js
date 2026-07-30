37bb70e774adbe68d249cdea43c1550533479c88
Kingmax12 <oyedeletope559@gmail.com>
2026-07-30 15:18:31 +0100

    Refactor Firebase initialization and configuration

--- function.js
+++ function.js
@@ -1,26 +1,27 @@
- import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  import {getAnalytics,
-    ref,
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  import { getAnalytics,
+          ref,
          push,
          onValue,
-         remove 
-  } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+         remove } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCNv-XOehSDf8OPtl2UpbBVnzN76frFRb4",
+    authDomain: "leads-tracker-app-44d59.firebaseapp.com",
+    projectId: "leads-tracker-app-44d59",
+    storageBucket: "leads-tracker-app-44d59.firebasestorage.app",
+    messagingSenderId: "487274490195",
+    appId: "1:487274490195:web:01f4f2f812709213a459a6",
+    measurementId: "G-STW01Q05HY"
+  };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCNv-XOehSDf8OPtl2UpbBVnzN76frFRb4",
-  authDomain: "leads-tracker-app-44d59.firebaseapp.com",
-  projectId: "leads-tracker-app-44d59",
-  storageBucket: "leads-tracker-app-44d59.firebasestorage.app",
-  messagingSenderId: "487274490195",
-  appId: "1:487274490195:web:305e7ddb4af507eea459a6",
-  measurementId: "G-3Q36PFPEYZ",
- databaseURL: "https://leads-tracker-app-44d59.firebaseapp.com"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 const referenceInDB = ref(analytics, "leads")
 
 const inputEl = document.getElementById("input-el")
